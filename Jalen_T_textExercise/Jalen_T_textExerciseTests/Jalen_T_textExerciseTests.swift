30700c5a65316ec05258db18a64b25c90409d807
jalen-cryptoHack <67969173+jalen-cryptoHack@users.noreply.github.com>
2020-08-11 18:13:20 -0500

    first commit
    first commit

---
+++ Jalen_T_textExercise/Jalen_T_textExerciseTests/Jalen_T_textExerciseTests.swift
@@ -0,0 +1,34 @@
+//
+//  Jalen_T_textExerciseTests.swift
+//  Jalen_T_textExerciseTests
+//
+//  Created by Jalen Taylor on 8/11/20.
+//  Copyright © 2020 Jalen Taylor. All rights reserved.
+//
+
+import XCTest
+@testable import Jalen_T_textExercise
+
+class Jalen_T_textExerciseTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
